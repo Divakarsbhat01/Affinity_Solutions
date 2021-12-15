951339e7cdf1dd2f9b78d9b4531d7ebfc0bfe874
Divakarsbhat01 <96167703+Divakarsbhat01@users.noreply.github.com>
2021-12-15 12:35:46 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,68 @@
+# ***  Assumptions made in the programe    ***
+# the input file given to me is an text file with an extension .txt
+# mildly profane words: idiot, stupid, donkey
+# medium profane words: shrewd, cunning, demagouge
+# harsh profane words: monkey, rat
+# filename is myfile.txt its the file in which the collection of twitter comments are located
+# the input file is placed in the same folder where the script/ code file is placed
+
+# *** Logic Explaination ***
+# first open the file in read mode
+# copy all the contents into a list (it automatically gets seperated by line)
+# remove all the '\n' characters
+# convert list to string
+# split the string at '.'(full stop) as problem constraint states that analysis will be done based on sentences
+# define what is mild, medium and harshly profane use dictionary with clearly defined words, their scores along with category
+# extract a sentence from a list
+# check for harsh, meduium and mildly profane words, once a category is discovered...
+#stop checking further and assign it to profanity value(prof in this program)
+#print the sentence with level of profanity in that language 
+
+
+def listtoString(s):                    # method declaration for conversion of list to string   
+    str1=""                             # declaring an empty string
+    for x in s:                         # take an element from the list
+        str1+=x                         # append it with the empty string
+    return str1                         # once the process is completed return the string
+f1=open("myfile.txt",'r')              # open the text file, file path, and r indiyates that i am opening the file in read mode
+line=f1.readlines()                     # read the complete text file into a list such that each element denotee corresponding line
+f1.close()                              # once all content is transfered from text file to list close the file
+i=0                                     # variable declaration used for iterating
+while i < len(line):                    # while the iterator is less than the length of the list
+    a=line[i].replace("\n","")          # access the variable of the list(specific line) and remove all "\n" characters and assign it to variable a
+    line[i]=a                           # assign variable a to the specific list variable in that position
+    i+=1                                # increment the iterator
+str2=listtoString(line)                 # convert list to string
+correct=str2.split(".")                 # as each sentences' profanity needs to be checked, as a sentence closes with a full stop we will divide the string into a list by splitting the string at '.' fullstop
+diy1={"idiot":1,"stupid":1,"donkey":1}  # profanity level1
+diy2={"shrewd":3,"cunning":3,"demagouge":3}# profanity level 2
+diy3={"monkey":10,"rat":10}                # profanity level 3
+prof=0                                  # profanity variable assigned to zero initially
+for i in correct:                      # access the element in the list 'correct'
+    prof=0                              # profanity variable assigned to zero initially
+    for word in diy3:                   # access the key value in the harsh diytionary
+        if word in i:                   # check if the word exists in the selected list value(sentence)
+            prof+=diy3[word]            # if exists then assign profainity word value to prof variable
+            break                       # once we have determined the hiegest level profainity in a sentence we dont need to go further
+        else:                           # if word does not exist then continue to next word in the diytionary
+            continue
+    for word in diy2:                   # access the key value in the medium diytionary
+        if word in i:                   # check if the word exists in the selected list value(sentence)
+            prof+=diy2[word]            # once we have determined the hiegest level profainity in a sentence we dont need to go further
+            break                       
+        else:                           # if word does not exist then continue to next word in the diytionary
+            continue
+    for word in diy1:                   # access the key value in the mild diytionary
+        if word in i:                   # check if the word exists in the selected list value(sentence)
+            prof+=diy1[word]            # once we have determined the hiegest level profainity in a sentence we dont need to go further
+        else:
+            continue                    # if word does not exist then continue to next word in the diytionary
+    if (prof>0)&(prof<2):               # if profanity is 1 then sentence contains mild profanity
+        print(i,"-- Sentence is Mild Profanity\n")  # print the premise with conclusion
+    elif (prof>2)&(prof<5):             # if profanity is 5 then sentence contains medium profanity
+        print(i,"-- Sentence is Medium Profanity\n")# print the premise with conclusion
+    elif (prof>9)&(prof<11):            # if profanity is 10 then sentence contains harsh profanity
+        print(i,"-- Sentence is Harsh Profanity\n") # print the premise with conclusion
+    elif (prof<1)&(i!=""):               # if profanity is 0 then sentence contains no profanity
+        print(i,"-- this sentence has no profanity\n") # print the premise with conclusion
+   
